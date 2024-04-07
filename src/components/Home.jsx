e53b2879daac73bee25ceafe02e357e2257eb74e
Anirudh <anirudhpatnaik2804@gmail.com>
2024-04-07 22:47:13 +0530

    added read page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 
 function Home() {
+
+  const navigate = useNavigate()
 
   const [data, setData] =  useState([]);
 
@@ -13,12 +15,26 @@ function Home() {
     .catch(err => console.log(err))
   },[])
 
+  const handleDelete = (id) => {
+    const confirm = window.confirm('The user will be deleted')
+    if(confirm){
+      axios.delete('http://localhost:3000/user/' + id)
+      .then(res => (
+        navigate('/')
+      ))
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+
+  }
+
   return (
     <div className="flex flex-col justify-center items-center bg-zinc-700 h-100">
       <h1 className="text-neutral-50 text-4xl my-4">List of Users</h1>
       <div className="w-auto rounded bg-white border shadow p-4 my-6">
         <div className="flex justify-end px-5 text-blue-700 ">
-          <Link className="bg-green-600 hover:bg-green-700 text-white font-bold py-1 px-3 rounded" to={"/create"}>Add User</Link>
+          <Link className="bg-green-600 hover:bg-green-700 text-white font-bold py-1 mb-1 px-3 rounded" to={"/create"}>Add User</Link>
         </div>
         <table className="w-full table-auto shadow">
           <thead >
@@ -40,8 +56,8 @@ function Home() {
                   <td className="px-3 py-2">{data.phone}</td>
                   <td className="px-3 py-2">
                     <Link to={`/read/${data.id}`} className="bg-blue-300 hover:bg-blue-400 rounded py-1 px-3 mx-1 ">Read</Link>
-                    <button className="bg-blue-400 hover:bg-blue-500 rounded py-1 px-3 mx-1 ">Edit</button>
-                    <button className="bg-red-400 hover:bg-red-500 rounded py-1 px-3 mx-1 ">Delete</button>
+                    <Link to={`/update/${data.id}`} className="bg-blue-400 hover:bg-blue-500 rounded py-1 px-3 mx-1 ">Edit</Link>
+                    <button onClick={(e) => {handleDelete(data.id)}} className="bg-red-400 hover:bg-red-500 rounded py-1 px-3 mx-1 ">Delete</button>
                   </td>
                 </tr>
               ))
--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,9 +1,107 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from "react-router-dom";
+
 
 function Update() {
+  // const [data, setData] = useState([]);
+  const { id } = useParams();
+  const [value, setValue] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/user/" + id)
+      .then((res) => {setValue(res.data)})
+      .catch((err) => console.log(err));
+  }, []);
+
+  const updateHandler = (event) => {
+    event.preventDefault()
+    axios.put('http://localhost:3000/user/' + id, value)
+    .then((res) => {
+      alert('User updated successfully!')
+      navigate('/')
+    })
+    
+
+  }
+
   return (
-    <div>
-      update
+    <div className="flex justify-center items-center h-screen bg-zinc-700">
+      <div className="bg-white shadow-md rounded px-8 pt-4 pb-4 mb-4 w-4/12">
+        <h1 className="block mb-2 text-xl font-medium text-gray-900 text-center ">
+          Edit User
+        </h1>
+        <form onSubmit={updateHandler} >
+          <div>
+            <label
+              className="block text-gray-700 text-sm font-bold mt-2"
+              htmlFor="name"
+            >
+              User Name
+            </label>
+            <input
+              value={value.name}
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="name"
+              placeholder="Jhon"
+              onChange={(e) => {setValue({...value, name: e.target.value})}}
+            />
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 text-sm font-bold mt-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              value={value.email}
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="email"
+              placeholder="abc@xyz.com"
+              onChange={(e) => {setValue({...value, email: e.target.value})}}
+            />
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 text-sm font-bold mt-2"
+              htmlFor="phone"
+            >
+              Phone
+            </label>
+            <input
+              value={value.phone}
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="phone"
+              placeholder="9563258456"
+              onChange={(e) => {setValue({...value, phone: e.target.value})}}
+            />
+          </div>
+          <div className="flex items-center justify-between py-3">
+            <button className="bg-green-600 hover:bg-green-700 text-white py-1 px-3 rounded  ">
+              Update
+            </button>
+            <Link
+              className="bg-blue-400 hover:bg-blue-500 rounded py-1 px-3 mx-1 "
+              to={"/"}
+            >
+              Back
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
